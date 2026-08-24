28e0f7b30359f26e4130e083b3d52a95b87a1733
chuahchengxi <boyboychuah@hotmail.com>
2026-08-24 14:37:44 +0800

    Croissaint 3.4.0

--- Casks/croissaint.rb
+++ Casks/croissaint.rb
@@ -1,8 +1,8 @@
 cask "croissaint" do
-  version "3.3.3-beta.2"
-  sha256 "1d3f26e63d2b78941170ac86d22fc1464e8d2480fa78148dae3f20713bb8f4cb"
+  version "3.4.0"
+  sha256 "20dc288a20a6148e517a46ae0af48fcdff656e16d9a85d2f32417506ad4e9ea6"
 
-  url "https://github.com/chuahchengxi/croissant/releases/download/v#{version}/Croissaint-#{version}.zip"
+  url "https://github.com/chuahchengxi/croissant/releases/download/v#{version}/Croissaint-#{version}.dmg"
   name "Croissaint"
   desc "Menu-bar companion: clipboard, window switching, screen tools and a desktop pet"
   homepage "https://github.com/chuahchengxi/croissant"
@@ -12,5 +12,5 @@ cask "croissaint" do
     strategy :github_latest
   end
 
-  app "Croissaint.app"
+  app "Croissant.app"
 end
