9a52d50f9d91b57c4246c9cf7d471bdbd2b849ce
chuahchengxi <boyboychuah@hotmail.com>
2026-08-24 20:19:04 +0800

    Repoint 3.4.1 sha256 to the CI-notarized DMG

--- Casks/croissaint.rb
+++ Casks/croissaint.rb
@@ -1,6 +1,6 @@
 cask "croissaint" do
   version "3.4.0"
-  sha256 "20dc288a20a6148e517a46ae0af48fcdff656e16d9a85d2f32417506ad4e9ea6"
+  sha256 "e7b497bec45e61f4535770d2fb3a8f1514a9af8527dac16e03adb598a3c3476b"
 
   url "https://github.com/chuahchengxi/croissant/releases/download/v#{version}/Croissaint-#{version}.dmg"
   name "Croissaint"
